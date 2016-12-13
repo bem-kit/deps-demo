7ee93ae664b2c4a71b5c54d5757110258db05622
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-12-14 01:06:58 +0300

    fix bugs. fix safari

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const declNormalize = require('bem-decl/lib/normalize2')
+const declNormalize = require('bem-decl/lib/normalize/v2')
 const nodeEval = require('node-eval')
 const inspect = require('util').inspect
 const $ = selector => document.querySelector(selector)
 const $$ = selector => Array.from(document.querySelectorAll(selector))
-const omit = require('lodash/omit')
+const _ = require('lodash')
 
 let stringify;
 const stringifiers = {
@@ -25,20 +25,26 @@ const $output = document.getElementById('output')
 const $examples = $('.examples')
 const $stringifierInspect = $('#stringify-inspect')
 const $stringifierJson = $('#stringify-json')
+const $layout = $('.layout')
 
 const log = args => ($status.innerHTML = args.toString(), $status.classList.remove('_ok'), $status.classList.remove('_error'))
 const error = args => ($status.innerHTML = args.toString(), $status.classList.remove('_ok'), $status.classList.add('_error'))
 const success = args => ($status.innerHTML = args.toString(), $status.classList.add('_ok'), $status.classList.remove('_error'))
 
-const onOutput = val => {console.info('ok'); localStorage.setItem('input', $input.value)}
+const normalize = value => declNormalize(nodeEval(value.trim()[0] === '{' ? `(${value})` : value))
 const output = val => {$output.value = val; onOutput(val)}
+
+const onOutput = val => {
+    console.info('ok');
+    localStorage.setItem('input', $input.value)
+}
 const onInput = () => {
     try {
-        const builtDeps = declNormalize(nodeEval(input.value.trim()[0] === '{' ? `(${$input.value})` : input.value))
-        const builtDeps_ = builtDeps.map(item => Object.assign({}, item.entity, omit(item, 'entity')));
-        builtDeps_.forEach(item => (item.tech || (delete item.tech)));
-        output(stringify(builtDeps_));
-    } catch(e) {console.error(e);}
+        const builtDeps = normalize($input.value)
+            .map(item => Object.assign({}, item.entity, _.omit(item, 'entity')))
+            .map(item => _.omitBy(item, _.isNull))
+        output(stringify(builtDeps))
+    } catch(e) {console.error(e)}
 }
 
 $stringifierJson.checked = localStorage.getItem('stringifier') === 'json'
@@ -62,8 +68,8 @@ $stringifierInspect.addEventListener('change', () => (syncStringifier('inspect')
 $stringifierJson.addEventListener('change', () => (syncStringifier('json'), onInput()))
 
 const onExitExamples = () => {
-    $examples.classList.add('_hidden');
-    $$('.main').forEach(el=>el.classList.remove('_inactive'));
+    $examples.classList.add('_hidden')
+    $layout.classList.remove('_inactive')
 }
 
 // todo: переписать на какой-нибудь популярный роутер
@@ -71,7 +77,7 @@ const go = (uri, link, shouldNotChangeHistory) => {
     uri = uri || '/'
     console.log('go', uri, link, shouldNotChangeHistory);
     switch(uri) {
-        case '/examples': history.pushState({}, 'examples', '#!'+uri); $examples.classList.remove('_hidden'); $$('.main').forEach(el=>el.classList.add('_inactive')); break;
+        case '/examples': history.pushState({}, 'examples', '#!'+uri); $examples.classList.remove('_hidden'); $layout.classList.add('_inactive'); break;
         case '/example': $input.value = link&&stringify(nodeEval(`(${link.dataset.content})`)); console.log('link.dataset.content', link.dataset.content); onInput(); console.log('done', link, uri); onExitExamples(); shouldNotChangeHistory || history.pushState({}, 'example', '/'); break;
         case '/': onExitExamples(); shouldNotChangeHistory || history.pushState({}, $('title').innerHTML, '#!'+uri); break;
         defalut: shouldNotChangeHistory || history.replaceState({},'404','#!/404')
@@ -81,7 +87,6 @@ const go = (uri, link, shouldNotChangeHistory) => {
 $$('a').forEach(a => a.onclick = e => {e.preventDefault(); go(new URL(e.target.href).pathname, e.target); return false})
 
 const initPage = () => {
-
     go(location.hash.replace('#!',''))
 }
 
@@ -91,6 +96,7 @@ window.onpopstate = () => {
 
 initPage()
 
+// esc - close examples
 window.addEventListener('keyup', e => {
     e.keyCode === 27 && location.hash === '#!/examples' && go('/')
 })
